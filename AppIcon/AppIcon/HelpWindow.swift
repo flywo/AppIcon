98753b9bff3afc4b0d25840a2c627b0e9c7b1361
YuHua <yuhua@dgg.net>
2021-01-22 11:25:57 +0800

    fix

---
+++ AppIcon/AppIcon/HelpWindow.swift
@@ -0,0 +1,20 @@
+//
+//  HelpWindow.swift
+//  AppIcon
+//
+//  Created by mac on 2021/1/22.
+//
+
+import SwiftUI
+
+struct HelpWindow: View {
+    var body: some View {
+        Text(TextTools.HelpText)
+    }
+}
+
+struct HelpWindow_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpWindow()
+    }
+}
